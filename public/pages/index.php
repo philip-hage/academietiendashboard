6789e40f2e222e5dae84ca2c255c3b046171a6d2
Gerwin012 <gerwindeheus@icloud.com>
2025-09-16 17:24:03 +0200

    headertje

--- public/pages/index.php
+++ public/pages/index.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<html lang='en'>
-<head>
-   <meta charset='UTF-8'>
-   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-   <title>Titel</title>
-   <script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
-   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
-</head>
+<?php include('../includes/header.php');?>
 
 
-<body>
-
+<div>
    <h1>Welkom to Academie Tien</h1>
+</div>
    
 
-</body> 
 
-
-
-</html>
+<?php include('../includes/footer.php');?>
+   
